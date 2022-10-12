e5b45374e646d430d7c43873e61caaaee73b9c8b
Kwel999 <105500624+kwel999@users.noreply.github.com>
2022-10-12 15:55:13 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import asyncio
-import amino
+import aminofix
 try:
     import colorama
 except ModuleNotFoundError:
@@ -15,11 +15,11 @@ init()
 print(
 print(Fore.RED+ Style.BRIGHT)
 print(pyfiglet.figlet_format("KWEL ATE YOUR PIZZA", font="big"))
-client = amino.AsyncClient()
+client = aminofix.AsyncClient()
 print("\t\033[1;32m Subscribe To \033[1;36mhttps://youtube.com/c/KWELATEYOURPIZZA\n\n")
 async def authorization():
     try:
-        await client.login(email=input("°•°•°Email >> "), password=input("°•°•°Password >> "))
+        await client.login(email=input("    °•°•°Email >> "), password=input("    °•°•°Password >> "))
     except Exception as e:
         print(e)
         await authorization()
@@ -30,7 +30,7 @@ async def choice_community():
         clients = (await client.sub_clients(start=0, size=100))
         for x, name in enumerate(clients.name, 1):
             print(f"{x}.{name}")
-        com_id = clients.comId[int(input("°•°•°Choice community >> ")) - 1]
+        com_id = clients.comId[int(input("    °•°•°Choice community >> ")) - 1]
         return com_id
     except ValueError as e:
         print(f"Error: {e}")
@@ -44,7 +44,7 @@ async def follow_spam(user_ids, sub_client):
 async def main():
     await authorization()
     com_id = await choice_community()
-    sub_client = amino.AsyncSubClient(comId=com_id, profile=client.profile)
+    sub_client = aminofix.AsyncSubClient(comId=com_id, profile=client.profile)
     user_ids = (await sub_client.get_online_users(start=0, size=100)).profile.userId
     task_count = int(input(" •°•°•°How many times >>  "))
     while True:
