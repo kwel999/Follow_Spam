5cfcc130d2cf52c36b59950ea53f197773775914
Kwel999 <105500624+kwel999@users.noreply.github.com>
2022-10-14 16:01:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import asyncio
-import aminofix
+import aminofix.asyncfix as aminofix
 try:
     import colorama
 except ModuleNotFoundError:
@@ -12,14 +12,13 @@ except ModuleNotFoundError:
     import pyfiglet
 from colorama import init, Fore, Back, Style
 init()
-print()
 print(Fore.RED+ Style.BRIGHT)
-print(pyfiglet.figlet_format("FOLLOW SPAM", font="big"))
-client = aminofix.AsyncClient()
-print("\t\033[1;32m Subscribe To \033[1;36mhttps://youtube.com/c/KWELATEYOURPIZZA\n\n")
+print(pyfiglet.figlet_format("KWEL ATE YOUR PIZZA", font="big"))
+client = aminofix.Client()
+print("\t\033[1;32m Follow Spam \033[1;36mKWEL \n\n")
 async def authorization():
     try:
-        await client.login(email=input("    °•°•°Email >> "), password=input("    °•°•°Password >> "))
+        await client.login(email=input("°•°•°Email >> "), password=input("°•°•°Password >> "))
     except Exception as e:
         print(e)
         await authorization()
@@ -30,7 +29,7 @@ async def choice_community():
         clients = (await client.sub_clients(start=0, size=100))
         for x, name in enumerate(clients.name, 1):
             print(f"{x}.{name}")
-        com_id = clients.comId[int(input("    °•°•°Choice community >> ")) - 1]
+        com_id = clients.comId[int(input("°•°•°Choice community >> ")) - 1]
         return com_id
     except ValueError as e:
         print(f"Error: {e}")
@@ -44,7 +43,7 @@ async def follow_spam(user_ids, sub_client):
 async def main():
     await authorization()
     com_id = await choice_community()
-    sub_client = aminofix.AsyncSubClient(comId=com_id, profile=client.profile)
+    aminofix.SubClient(comId=com_id, profile=client.profile)
     user_ids = (await sub_client.get_online_users(start=0, size=100)).profile.userId
     task_count = int(input(" •°•°•°How many times >>  "))
     while True:
